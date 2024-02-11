fecba162dbfddc1911d05e70d851ff62f3985e6a
khizarshah01 <khizar.shah010@gmail.com>
2024-02-11 18:39:56 +0530

    input and button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,32 +28,43 @@ function Home() {
 
   return (
     <div className="mainCon">
-      <div className="Heading">
-        <h1 data-text="+JADU SUMARIZER+" >+JADU SUMARIZER+</h1>
-      </div>
+      <div className="head">
+        <div className="Heading">
+          <h1 data-text="+JADU SUMARIZER+">+JADU SUMARIZER+</h1>
+        </div>
         <h2>
           A web app that convert your large amount of text data into short text.
         </h2>
         <h3>NO SINGUP | FREE TO USE | OPEN SOURCE</h3>
-      <div className="con">
-        <div className="inputBox">
+      </div>
+
+      {/* second container */}
+      <div class="con">
+        <div class="inputBox">
           <input
+            id="Urle"
             type="text"
             value={url}
             onChange={(e) => setUrl(e.target.value)}
             required="required"
             placeholder="Enter URL"
           />
-          <label htmlFor="length" className="Heading2">Summary Length: {length}</label>
-          <input
-            type="range"
-            id="length"
-            min="1"
-            max="10"
-            value={length}
-            onChange={(e) => setLength(e.target.value)}
-          />
-          <button onClick={handleSummarize}>Summarize</button>
+           <button className="button-36" onClick={handleSummarize}>Summarize</button>
+
+          <div className="scon">
+            <input
+              type="range"
+              id="length"
+              min="1"
+              max="10"
+              value={length}
+              onChange={(e) => setLength(e.target.value)}
+            />
+           
+          </div>
+        </div>
+
+        <div class="textareaa">
           <textarea
             id="summarizedText"
             rows="10"
