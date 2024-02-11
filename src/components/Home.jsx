d929f180bd919b95489fd4cba289fbaac189f277
khizarshah01 <khizar.shah010@gmail.com>
2024-02-11 22:01:34 +0530

    style for text area

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Home.css";
 
 function Home() {
   const [url, setUrl] = useState("");
   const [length, setLength] = useState(5); // initial summary length set to 5
   const [summarizedText, setSummarizedText] = useState("");
+  const textAreaRef = useRef(null);
 
   const handleSummarize = (event) => {
     event.preventDefault();
@@ -26,6 +27,11 @@ function Home() {
       });
   };
 
+  const copyToClipboard = () => {
+    textAreaRef.current.select();
+    document.execCommand('copy');
+  };
+
   return (
     <div className="mainCon">
       <div className="head">
@@ -35,12 +41,12 @@ function Home() {
         <h2>
           A web app that convert your large amount of text data into short text.
         </h2>
-        <h3>NO SINGUP | FREE TO USE | OPEN SOURCE</h3>
+        <h3>NO SINGUP  |  FREE TO USE  |  OPEN SOURCE</h3>
       </div>
 
       {/* second container */}
-      <div class="con">
-        <div class="inputBox">
+      <div className="con">
+        <div className="inputBox">
           <input
             id="Urle"
             type="text"
@@ -49,28 +55,30 @@ function Home() {
             required="required"
             placeholder="Enter URL"
           />
-           <button className="button-36" onClick={handleSummarize}>Summarize</button>
+          <button className="button-36" onClick={handleSummarize}>Summarize</button>
 
           <div className="scon">
+            <h3>Length: </h3>
             <input
               type="range"
               id="length"
               min="1"
-              max="10"
+              max="15"
               value={length}
               onChange={(e) => setLength(e.target.value)}
             />
-           
           </div>
         </div>
 
-        <div class="textareaa">
+        <div className="textareaa">
           <textarea
             id="summarizedText"
             rows="10"
             cols="70"
             value={summarizedText}
+            ref={textAreaRef}
           ></textarea>
+          <div id="copyToClipboard-a" className="clipboard icon" onClick={copyToClipboard}></div>
         </div>
       </div>
     </div>
